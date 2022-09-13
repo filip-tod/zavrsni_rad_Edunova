5101cd8be7e58b5028580ef0c51b2b19d1dee37c
Filip Todorovic <filip.tod95@gmail.com>
2022-09-13 10:34:03 +0200

    copy-mvca

---
+++ app.hr/app/konfiguracija.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'url'=>'http://app.hr/',
+    'nazivApp'=>'Edunova App',
+    'baza'=>[
+        'server'=>'localhost',
+        'baza'=>'edunovapp25',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ]
+];
